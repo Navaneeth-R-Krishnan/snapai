49f303bd0e73c5b68b5d47595097c6c412a0d617
jahid-md-zysk <rnavaneeth19@gmail.com>
2025-02-24 20:55:23 +0530

    docs: update

--- image-gen/apps/backend/index.ts
+++ image-gen/apps/backend/index.ts
@@ -4,6 +4,26 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+app.post("/ai/training",(req,res)=>{
+
+})
+
+app.post("/ai/generate",(req,res)=>{
+
+})
+
+app.post("pack/generate",(req,res)=>{
+
+})
+
+app.get("/pack/bulk",(req,res)=>{
+    
+})
+
+app.get("/image",(req,res)=>{
+    
+})
+
 app.listen(PORT,()=>{
     console.log("Server is on 3000");
 });
